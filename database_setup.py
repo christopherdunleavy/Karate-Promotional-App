0349522bfc137733fbdbe7bfdebc72776d4090df
Your Name <ddrisgood137@gmail.com>
2017-09-27 23:33:05 -0700

    add pairings

--- database_setup.py
+++ database_setup.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Float, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.ext.hybrid import hybrid_property
 
 Base = declarative_base()
 
@@ -11,7 +12,7 @@ class Promotional(Base):
     id = Column(Integer, primary_key=True)
     date = Column(String(20), nullable=False)
     type = Column(String(20), nullable=False)
-
+    applications = relationship("Application", back_populates="promotional")
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
@@ -31,7 +32,11 @@ class Application(Base):
     rank = Column(String(20), nullable=False)
     color = Column(String(20), nullable=False)
     promotional_id = Column(Integer, ForeignKey('promotional.id'))
-    promotional = relationship(Promotional)
+    promotional = relationship("Promotional", back_populates="applications")
+
+    @hybrid_property
+    def fullName(self):
+        return self.firstName + " " + self.lastName
 
     @property
     def serialize(self):
@@ -40,65 +45,76 @@ class Application(Base):
             'id': self.id,
         }
 
-# class User(Base):
-#     __tablename__ = 'user'
+class Pairing(Base):
+    __tablename__ = 'pairing'
 
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-#     email = Column(String(250), nullable=False)
-#     picture = Column(String(250))
+    id = Column(Integer, primary_key=True)
+    promotional_id = Column(Integer, ForeignKey('promotional.id'))
+    color = Column(String(20), nullable=False)
+    sideA_id = Column(Integer, ForeignKey('application.id'))
+    sideB_id = Column(Integer, ForeignKey('application.id'))
+    application_A = relationship("Application", foreign_keys=[sideA_id], backref="pairingA")
+    application_B = relationship("Application", foreign_keys=[sideB_id], backref="pairingB")
+    promotional = relationship("Promotional")
 
-
-# class Publisher(Base):
-#     __tablename__ = 'publisher'
-
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-#     user_id = Column(Integer, ForeignKey('user.id'))
-#     user = relationship(User)
-
-#     @property
-#     def serialize(self):
-#         """Return object data in easily serializeable format"""
-#         return {
-#             'name': self.name,
-#             'id': self.id,
-#         }
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 
-# class Game(Base):
-#     __tablename__ = 'game'
+class Publisher(Base):
+    __tablename__ = 'publisher'
 
-#     name = Column(String(80), nullable=False)
-#     id = Column(Integer, primary_key=True)
-#     description = Column(String(250))
-#     min_players = Column(Integer)
-#     max_players = Column(Integer)
-#     price = Column(Float)
-#     min_length = Column(Integer)
-#     max_length = Column(Integer)
-#     publisher_id = Column(Integer, ForeignKey('publisher.id'))
-#     publisher = relationship(Publisher)
-#     user_id = Column(Integer, ForeignKey('user.id'))
-#     user = relationship(User)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-#     @property
-#     def serialize(self):
-#         """Return object data in easily serializeable format"""
-#         return {
-#             'name': self.name,
-#             'description': self.description,
-#             'id': self.id,
-#             'price': self.price,
-#             'min_players': self.min_players,
-#             'max_players': self.max_players,
-#             'min_length': self.min_length,
-#             'max_length': self.max_length,
-#             'publisher': self.publisher.name
-#         }
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
 
-engine = create_engine('sqlite:///boardgamecatalog.db')
+class Game(Base):
+    __tablename__ = 'game'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    min_players = Column(Integer)
+    max_players = Column(Integer)
+    price = Column(Float)
+    min_length = Column(Integer)
+    max_length = Column(Integer)
+    publisher_id = Column(Integer, ForeignKey('publisher.id'))
+    publisher = relationship(Publisher)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'min_players': self.min_players,
+            'max_players': self.max_players,
+            'min_length': self.min_length,
+            'max_length': self.max_length,
+            'publisher': self.publisher.name
+        }
+
+
+engine = create_engine('sqlite:///promotional.db')
 
 
 Base.metadata.create_all(engine)
--- project.py
+++ project.py
@@ -1,7 +1,10 @@
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from PyPDF2 import PdfFileReader, PdfFileWriter
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Promotional, Application
+from database_setup import Base, Promotional, Application, Pairing
 from flask import session as login_session
 import random
 import string
@@ -11,6 +14,7 @@ import httplib2
 import json
 from flask import make_response
 import requests
+import StringIO
 
 app = Flask(__name__)
 
@@ -20,7 +24,7 @@ APPLICATION_NAME = "Board Game Catalog Application"
 
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///boardgamecatalog.db')
+engine = create_engine('sqlite:///promotional.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -56,8 +60,220 @@ def showPromotional(promotional_id):
 def showPromotionalColor(promotional_id, color):
     promotional = session.query(Promotional).filter_by(id=promotional_id).one()
     applications = session.query(Application).filter_by(promotional_id=promotional_id, color=color).order_by(Application.lastName).all()
+    unmatchedApplications = session.query(Application).filter_by(promotional_id=promotional_id, color=color, pairingA=None, pairingB=None).order_by(Application.lastName).all()
+    existingPairings = session.query(Pairing).filter_by(promotional_id=promotional_id, color=color).all()
     title = promotional.date + " - " + promotional.type + ": " + color
-    return render_template('promotional.html', title=title, promotional_id=promotional_id, applications=applications)
+
+    def makeSelect(selected, name):
+        if selected != None:
+            select = "<select name='%s'>" % name
+            endSelect = "</select>"
+
+            for application in applications:
+                fullName = application.fullName
+                value = application.id
+                if application == selected:
+                    option = "<option value='%s' selected>%s</option>" % (value, fullName)
+                else:
+                    option = "<option value='%s'>%s</option>" % (value, fullName)
+                select = select + option
+
+            select = select + endSelect
+        else:
+            select = None
+        return select
+    
+    pairings = []
+    for pairing in existingPairings:
+        sideA = pairing.application_A#session.query(Application).filter_by(id=pairing.sideA_id).one()
+        # if pairing.sideB_id != None:
+        #     sideB = session.query(Application).filter_by(id=pairing.sideB_id).one()
+        # else:
+        #     sideB = None
+        sideB = pairing.application_B
+
+        if sideB == None and len(unmatchedApplications) > 0:
+            sideB = unmatchedApplications.pop(0)
+
+        tup = {"sideA":makeSelect(sideA, str(existingPairings.index(pairing)) + "sideA"),"sideB":makeSelect(sideB, str(existingPairings.index(pairing)) + "sideB")}
+        print tup
+        pairings.append(tup)
+
+    if len(unmatchedApplications) > 0:
+        i = 0
+        name = len(existingPairings)
+        while i < len(unmatchedApplications):
+            tup = {"sideA":makeSelect(unmatchedApplications[i], str(name) + "sideA"),"sideB":makeSelect(unmatchedApplications[i+1], str(name) + "sideB") if i+1 < len(unmatchedApplications) else None}
+            i += 2
+            name += 1
+            pairings.append(tup)
+
+    return render_template('promotional.html', title=title, promotional_id=promotional_id, applications=applications, color=color, pairings=pairings)
+
+
+
+@app.route('/<int:promotional_id>/<string:color>/certificates', methods=['GET', 'POST'])
+def generateCertificates(promotional_id, color):
+    promotional = session.query(Promotional).filter_by(id=promotional_id).one()
+    #yellow = session.query(Application).filter_by(promotional_id=promotional_id, color="yellow").order_by(Application.lastName).all()
+    applications = session.query(Application).filter_by(promotional_id=promotional_id, color=color).order_by(Application.lastName).all()
+    title = promotional.date + " - " + promotional.type + ": " + color
+
+    output = PdfFileWriter()
+    
+    date = promotional.date
+
+    for application in applications:
+        name = application.firstName + " " + application.lastName
+        certificate = PdfFileReader(open("promotionalCertificate.pdf", "rb"))
+        certificatePage = certificate.getPage(0)
+        
+        infoBuffer = StringIO.StringIO()
+        
+        def hello(c):
+            c.setFont('Helvetica', 24)
+            c.drawCentredString(305,452, name)
+            c.setFont('Helvetica', 36)
+            c.drawCentredString(305,372, application.rank)
+            c.setFont('Helvetica', 15)
+            c.drawCentredString(210,330, date)
+            c.drawCentredString(200,285,"Sensei Sue, Sensei Nobu")
+
+        c = canvas.Canvas(infoBuffer)
+
+        hello(c)
+        c.showPage()
+        c.save()
+        
+        infoBuffer.seek(0)
+        info = PdfFileReader(infoBuffer)
+        certificatePage.mergePage(info.getPage(0))
+        output.addPage(certificatePage)
+        infoBuffer.close()
+
+    outputStream = StringIO.StringIO()
+    output.write(outputStream)
+
+    pdfOut = outputStream.getvalue()
+    outputStream.close()
+    
+    fileName = color + " certificates.pdf"
+    
+    response = make_response(pdfOut)
+    response.headers['Content-Disposition'] = "attachment; filename=" + fileName
+    response.mimetype = 'application/pdf'
+    return response
+
+    #return render_template('promotional.html', title=title, promotional_id=promotional_id, applications=applications, color=color)
+
+@app.route('/<int:promotional_id>/<string:color>/judgesPackets', methods=['GET', 'POST'])
+def generateJudgesPackets(promotional_id, color):
+    promotional = session.query(Promotional).filter_by(id=promotional_id).one()
+    #yellow = session.query(Application).filter_by(promotional_id=promotional_id, color="yellow").order_by(Application.lastName).all()
+    applications = session.query(Application).filter_by(promotional_id=promotional_id, color=color).order_by(Application.lastName).order_by(Application.rank).all()
+    title = promotional.date + " - " + promotional.type + ": " + color
+
+    output = PdfFileWriter()
+    infoBuffer = StringIO.StringIO()
+    c = canvas.Canvas(infoBuffer)
+    date = promotional.date
+    judgesPacket = PdfFileReader(open("RankingSheet.pdf", "rb"))
+    judgesPacketPage = judgesPacket.getPage(0)
+    counter = 0
+    previousRank = applications[0].rank
+
+    for application in applications:
+        name = application.firstName + " " + application.lastName
+
+        if counter == 4 or previousRank != application.rank:
+            counter = 0
+            c.showPage()
+            c.save()
+
+            infoBuffer.seek(0)
+            info = PdfFileReader(infoBuffer)
+            judgesPacketPage.mergePage(info.getPage(0))
+            output.addPage(judgesPacketPage)
+            infoBuffer.close()
+
+            infoBuffer = StringIO.StringIO()
+            c = canvas.Canvas(infoBuffer)
+            judgesPacket = PdfFileReader(open("RankingSheet.pdf", "rb"))
+            judgesPacketPage = judgesPacket.getPage(0)
+
+        if counter == 0:
+            c.setFont('Helvetica', 24)
+            c.drawCentredString(300, 700, application.rank + " " + color.title() + " Belt")
+            c.setFont('Helvetica', 12)
+            c.drawCentredString(450, 566, name)
+            counter += 1
+        elif counter == 1:
+            c.setFont('Helvetica', 12)
+            c.drawCentredString(450, 417, name)
+            counter += 1
+        elif counter == 2:
+            c.setFont('Helvetica', 12)
+            c.drawCentredString(450, 270, name)
+            counter += 1
+        elif counter == 3:
+            c.setFont('Helvetica', 12)
+            c.drawCentredString(450, 115, name)
+            counter += 1
+
+        if applications.index(application) == len(applications)-1:
+            counter = 0
+            c.showPage()
+            c.save()
+
+            info = PdfFileReader(infoBuffer)
+            judgesPacketPage.mergePage(info.getPage(0))
+            output.addPage(judgesPacketPage)
+
+            infoBuffer = StringIO.StringIO()
+
+        previousRank = application.rank
+        # infoBuffer.seek(0)
+        # info = PdfFileReader("output.pdf")
+        # judgesPacketPage.mergePage(info.getPage(0))
+        # output.addPage(judgesPacketPage)
+        #infoBuffer.close()
+
+    outputStream = StringIO.StringIO()
+    output.write(outputStream)
+
+    pdfOut = outputStream.getvalue()
+    outputStream.close()
+
+    fileName = color + " judgesPacket.pdf"
+
+    response = make_response(pdfOut)
+    response.headers['Content-Disposition'] = "attachment; filename=" + fileName
+    response.mimetype = 'application/pdf'
+    return response
+    #return render_template('promotional.html', title=title, promotional_id=promotional_id, applications=applications, color=color)
+
+@app.route('/<int:promotional_id>/<string:color>/pairings', methods=['GET', 'POST'])
+def updatePairings(promotional_id, color):
+    if request.method == 'POST':
+        print request.form
+        session.query(Pairing).delete()
+
+        i = 0
+        while i <= len(request.form)/2:
+            sideA_id=request.form[str(i)+"sideA"]
+
+            if (str(i)+"sideB") in request.form:
+                sideB_id=request.form[str(i)+"sideB"] 
+            else:
+                sideB_id = None
+            print sideB_id
+            newPairing = Pairing(promotional_id=promotional_id, sideA_id=sideA_id, sideB_id=sideB_id, color=color)
+            session.add(newPairing)
+            i += 1
+
+        session.commit()
+        return redirect(url_for('showPromotionalColor', promotional_id=promotional_id, color=color))
+
 
 @app.route('/<int:promotional_id>/addApplication', methods=['GET', 'POST'])
 def addApplication(promotional_id):
@@ -252,7 +468,7 @@ def gconnect():
     # Verify that the access token is valid for this app.
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
-            json.dumps("Token's client ID does not match app's."), 401)
+                json.dumps("Token's client ID does not match app's."), 401)
         print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
