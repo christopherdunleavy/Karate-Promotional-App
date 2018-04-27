805b347dfb60dc31ef95ba9ce7576a2c4a64b48c
Your Name <ddrisgood137@gmail.com>
2018-04-27 13:55:18 -0700

    fix:add app config

--- project.py
+++ project.py
@@ -651,5 +651,6 @@ def rank_to_belt(rank):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
